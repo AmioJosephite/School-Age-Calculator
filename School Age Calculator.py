328243c5f996aa5e59273206a0f424677476e91d
AmioJosephite <82232417+AmioJosephite@users.noreply.github.com>
2021-04-09 23:44:17 +0600

    Add files via upload

---
+++ School Age Calculator.py
@@ -0,0 +1,11 @@
+def School_Age_Calculator(age):
+    if age < 5:
+        print("Enjoy the time!")
+    
+    elif age == 7:
+        print("Enjoy Kindergarten")
+    
+    else: 
+        print("They grow up so fast!")
+
+School_Age_Calculator(1)
